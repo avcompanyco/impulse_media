b23cab057d5d17c0c3fba392a2ec5215f201f765
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2025-10-31 00:42:07 -0600

    fix: build

--- public/build/assets/DashboardCard-BTTNM7Rc.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-D3BaTrhN.js";import"./app-BCSTVVYZ.js";export{m as default};
---
+++ public/build/assets/DashboardCard-CDZ894mI.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-b2JXUTSg.js";import"./app-C3dMsmAt.js";export{m as default};

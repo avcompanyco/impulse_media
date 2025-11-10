b7b2cf74a4eb367616e59d005fd583b45cd10097
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2025-11-10 16:25:40 -0600

    fix: Math.ceil -> Math.floor

--- public/build/assets/DashboardCard-BV7e1KUb.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-DsoV7bZF.js";import"./app-DNZh6KpH.js";export{m as default};
---
+++ public/build/assets/DashboardCard-omEbMOIk.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-DW_MYvtt.js";import"./app-B-X8UrwJ.js";export{m as default};

dc7f6aa74945ffd8402bab24c206d3119a46a826
Antonio Varona <support@impulsemedia.me>
2026-07-22 16:43:33 -0500

    Add instant SVG arrow navigation and PPV badges to Suggested section in Movies and Series

--- public/build/assets/DashboardCard-BHCHEHz-.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-BtmsqCtY.js";import"./app-DFtrte_4.js";export{m as default};
---
+++ public/build/assets/DashboardCard-DAqnX4R3.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DashboardCard.vue_vue_type_style_index_0_lang-nR9Yeawk.js";import"./app-CBxfIKq3.js";export{m as default};
